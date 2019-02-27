a2a308f1cf83afc65e213c85b69f1a49eb105028
Mitch-SB <mitch_stbrmdz@hotmail.com>
2019-02-27 18:28:02 -0500

    Implement a ViewModel

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -14,6 +15,24 @@ namespace Vidly.Controllers
         {
             var movies = GetMovies();
             return View(movies);
+        }
+
+        public ActionResult Random()
+        {
+            var movie = new Movie { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
 
         public IEnumerable<Movie> GetMovies()
