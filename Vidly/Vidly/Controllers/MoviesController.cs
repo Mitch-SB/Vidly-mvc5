e846f4cf4f43f73d0639a89d123d92c15cda9f5d
Mitch-SB <mitch_stbrmdz@hotmail.com>
2019-02-27 22:25:30 -0500

    Create Movies and Genres table
    Display the list of movies and their details

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -5,15 +5,28 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()   
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();   
+        }
+        
         // GET: Movies
         public ActionResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
             return View(movies);
         }
 
@@ -34,19 +47,10 @@ namespace Vidly.Controllers
 
             return View(viewModel);
         }
-
-        public IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Shrek"},
-                new Movie {Id = 2, Name = "Megamind"}
-            };
-        }
-        
+                
         public ActionResult Details(int id)
         {
-            var movies = GetMovies().SingleOrDefault(m => m.Id == id);
+            var movies = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
 
             if(movies == null)
             {
